e31e246651e0cb0104560ab9ab2887849945869e
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-11-18 23:29:43 +0530

    fin

--- IG_Testing/routes/users.js
+++ IG_Testing/routes/users.js
@@ -3,7 +3,7 @@ const plm = require("passport-local-mongoose");
 
 mongoose
   .connect(
-    ""
+    "mongodb+srv://admin:FpJ6gEYC7z0JPHjd@cluster0.gz2nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("Database connection established"))
   .catch(() => console.log("Database connection establishment failed"));
